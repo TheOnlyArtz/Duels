309fb7f85d7f7734065d63c9982665450e657e13
callcraft456@gmail.com <callcraft456@gmail.com>
2019-06-29 19:12:03 +0300

    Added basic properties to ConfigManager

--- src/me/theonlyartz/oitc/managers/ConfigManager.java
+++ src/me/theonlyartz/oitc/managers/ConfigManager.java
@@ -1,4 +1,16 @@
 package me.theonlyartz.oitc.managers;
 
+import me.theonlyartz.oitc.Main;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+
 public class ConfigManager {
+    public File file = new File("/Plugins/OITC/", "config.yaml");
+    public YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
+    private Main plugin;
+
+    public ConfigManager(Main plugin) {
+        this.plugin = plugin;
+    }
 }
