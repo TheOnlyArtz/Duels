7a4f0e7b803043350a7fbe1c5f87d1ee9f19357c
callcraft456@gmail.com <callcraft456@gmail.com>
2019-06-29 23:43:31 +0300

    Integrated gamesManager and added a sendMessage with prefix

--- src/me/theonlyartz/oitc/Main.java
+++ src/me/theonlyartz/oitc/Main.java
@@ -2,13 +2,26 @@ package me.theonlyartz.oitc;
 
 import me.theonlyartz.oitc.commands.OitcCommand;
 import me.theonlyartz.oitc.events.Events;
+import me.theonlyartz.oitc.managers.GamesManager;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+    public GamesManager gamesManager;
+
+    public Main() {
+        this.gamesManager = new GamesManager(this);
+    }
+
+    public static void sendMessage(Player p, String msg) {
+        String toSend = ChatColor.translateAlternateColorCodes('&', "&2&O[OneInTheChamber]&2&O " + msg);
+        p.sendMessage(toSend);
+    }
 
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new Events(this), this);
-        this.getCommand("oitc").setExecutor(new OitcCommand());
+        this.getCommand("oitc").setExecutor(new OitcCommand(this));
     }
 }
