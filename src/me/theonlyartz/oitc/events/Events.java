18bbf88d137f855e4350da757f03bf3e0a35b8f7
callcraft456@gmail.com <callcraft456@gmail.com>
2019-06-30 00:08:22 +0300

    A new event for player death

--- src/me/theonlyartz/oitc/events/Events.java
+++ src/me/theonlyartz/oitc/events/Events.java
@@ -1,10 +1,37 @@
 package me.theonlyartz.oitc.events;
 
 import me.theonlyartz.oitc.Main;
+import me.theonlyartz.oitc.managers.GameInstanceManager;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class Events implements Listener {
-    public Events(Main plugin) {
+    private Main plugin;
 
+    public Events(Main p) {
+        this.plugin = p;
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent e) {
+        Player player = e.getEntity();
+        GameInstanceManager game = this.plugin.gamesManager.lookUpPlayer(player.getName());
+        if (game == null) return;
+
+        if (!(player.getKiller() instanceof Player)) {
+            game.getPlayerTeam(player).respawnPlayer(player);
+            String msg = String.format("&O&B%s&r &3just died by some unknown shit!", player.getName());
+            game.teamB.broadcast(msg);
+            game.teamA.broadcast(msg);
+            return;
+        }
+
+        Player killer = player.getKiller();
+
+        String msg = String.format("&O&B%s&r &3has been killed by&r &L&6%s", player.getName(), killer.getName());
+        game.teamB.broadcast(msg);
+        game.teamA.broadcast(msg);
     }
 }
