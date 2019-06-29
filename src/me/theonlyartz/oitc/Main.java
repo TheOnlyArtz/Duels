7e51824d34be6e9341d536549b66d730aa23c841
callcraft456@gmail.com <callcraft456@gmail.com>
2019-06-29 19:02:32 +0300

    Add a new command and assign it

--- src/me/theonlyartz/oitc/Main.java
+++ src/me/theonlyartz/oitc/Main.java
@@ -1,5 +1,6 @@
 package me.theonlyartz.oitc;
 
+import me.theonlyartz.oitc.commands.OitcCommand;
 import me.theonlyartz.oitc.events.Events;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -8,5 +9,6 @@ public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new Events(this), this);
+        this.getCommand("oitc").setExecutor(new OitcCommand());
     }
 }
---
+++ src/me/theonlyartz/oitc/commands/OitcCommand.java
@@ -0,0 +1,18 @@
+package me.theonlyartz.oitc.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class OitcCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("You are not allowed to do that!");
+            return true;
+        }
+        return true;
+    }
+}
