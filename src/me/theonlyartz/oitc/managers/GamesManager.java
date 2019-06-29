60cb287b109ab106930ea56442c7d9ceadc0f757
callcraft456@gmail.com <callcraft456@gmail.com>
2019-06-29 21:46:18 +0300

    Added basic utilities to the GameManager

--- src/me/theonlyartz/oitc/managers/GamesManager.java
+++ src/me/theonlyartz/oitc/managers/GamesManager.java
@@ -1,9 +1,13 @@
 package me.theonlyartz.oitc.managers;
 
 import me.theonlyartz.oitc.Main;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class GamesManager {
     public ArrayList<ArrayList<Location>> reservedCords = new ArrayList<ArrayList<Location>>();
@@ -13,5 +17,51 @@ public class GamesManager {
 
     public GamesManager(Main plugin) {
         this.plugin = plugin;
+
+        Location one = new Location(Bukkit.getWorld("world"), -10.5, 80, 32.5);
+        Location two = new Location(Bukkit.getWorld("world"), -8.5, 80, -20.5);
+        availableCords.add(new ArrayList<Location>(Arrays.asList(one, two)));
+    }
+
+    public HashMap<String, Location> reserveCords() {
+        ArrayList<Location> toAdd = availableCords.get(0);
+        HashMap<String, Location> locations = new HashMap<>();
+
+        locations.put("A", toAdd.get(0));
+        locations.put("B", toAdd.get(1));
+
+        reservedCords.add(toAdd);
+        availableCords.remove(toAdd);
+
+        return locations;
+    }
+
+    public void unreserveCords(ArrayList<Location> locations) {
+        availableCords.add(locations);
+        reservedCords.remove(locations);
+        return;
+    }
+
+    public ArrayList<Location> hashToListOfLocations(HashMap<String, Location> map) {
+        ArrayList<Location> locations = new ArrayList<>();
+
+        locations.add(map.get("A"));
+        locations.add(map.get("B"));
+
+        return locations;
+    }
+
+    public Player lookUpPlayer(String name) {
+        for (GameInstanceManager game : runningGames) {
+            for (Player p : game.teamA.getPlayers()) {
+                if (p.getName().equals(name)) return p;
+            }
+
+            for (Player p : game.teamB.getPlayers()) {
+                if (p.getName().equals(name)) return p;
+            }
+        }
+
+        return null;
     }
 }
