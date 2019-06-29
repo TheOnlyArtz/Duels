3f74e11bbfed5ebcc3decc3e074d0b280353254b
callcraft456@gmail.com <callcraft456@gmail.com>
2019-06-30 00:53:44 +0300

    Add dammage mechanics (one shot one kill bow)

--- src/me/theonlyartz/oitc/events/Events.java
+++ src/me/theonlyartz/oitc/events/Events.java
@@ -2,10 +2,19 @@ package me.theonlyartz.oitc.events;
 
 import me.theonlyartz.oitc.Main;
 import me.theonlyartz.oitc.managers.GameInstanceManager;
+import net.minecraft.server.v1_8_R3.PacketPlayInClientCommand;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class Events implements Listener {
     private Main plugin;
@@ -25,13 +34,43 @@ public class Events implements Listener {
             String msg = String.format("&O&B%s&r &3just died by some unknown shit!", player.getName());
             game.teamB.broadcast(msg);
             game.teamA.broadcast(msg);
+            game.getPlayerTeam(player).respawnPlayer(player);
             return;
         }
 
         Player killer = player.getKiller();
-
+        killer.getInventory().addItem(new ItemStack(Material.ARROW, 1));
         String msg = String.format("&O&B%s&r &3has been killed by&r &L&6%s", player.getName(), killer.getName());
         game.teamB.broadcast(msg);
         game.teamA.broadcast(msg);
+        game.getPlayerTeam(player).respawnPlayer(player);
+    }
+
+    @EventHandler
+    public void onArrowHit(EntityDamageByEntityEvent e) {
+        if (e.getEntity() instanceof Player && e.getDamager() instanceof Player) {
+            Player whoWasHit = (Player) e.getEntity();
+            Player whoHit = (Player) e.getDamager();
+            GameInstanceManager game = this.plugin.gamesManager.lookUpPlayer(whoWasHit.getName());
+            if (game == null) return;
+
+            if (game.getPlayerTeam(whoWasHit).players.contains(whoHit)) {
+                e.setCancelled(true);
+            }
+        }
+
+        if ((e.getDamager() instanceof Projectile)) {
+            if (e.getDamager().getType().equals(EntityType.ARROW)) {
+                Player player = (Player) (((Projectile) e.getDamager()).getShooter());
+                GameInstanceManager game = this.plugin.gamesManager.lookUpPlayer(player.getName());
+                if (game != null) {
+                    if ((e.getEntity() instanceof Player) && game.getPlayerTeam((Player) e.getEntity()).players.contains(player)) {
+                        e.setCancelled(true);
+                        return;
+                    }
+                    e.setDamage(1000);
+                }
+            }
+        }
     }
 }
