7ea9e11b1d47abfc89aaa40ddec29d7d580fd889
callcraft456@gmail.com <callcraft456@gmail.com>
2019-06-29 21:14:50 +0300

    Added some new utilities to the Team model

---
+++ src/me/theonlyartz/oitc/models/Team.java
@@ -0,0 +1,49 @@
+package me.theonlyartz.oitc.models;
+
+import me.theonlyartz.oitc.Main;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+
+public class Team {
+    public ArrayList<Player> players;
+    public Location spawnPoint;
+    private Main plugin;
+
+    public Team(Main p) {
+        this.plugin = p;
+    }
+
+    public void addPlayer(Player p) {
+        players.add(p);
+    }
+
+    public void removePlayer(Player p) {
+        players.remove(p);
+    }
+
+    public void spawn() {
+        for (Player p : this.players) {
+            p.teleport(this.spawnPoint);
+        }
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return this.players;
+    }
+
+    public Location getSpawnPoint() {
+        return this.spawnPoint;
+    }
+
+    public void setSpawnPoint(Location point) {
+        this.spawnPoint = point;
+    }
+
+    public void broadcast(String msg) {
+        for (Player p : this.players) {
+            p.sendMessage(msg);
+        }
+    }
+}
