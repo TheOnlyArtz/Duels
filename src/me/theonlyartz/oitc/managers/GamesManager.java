70cd8f54df09a9d709a4fc13dc4015ba753777fc
callcraft456@gmail.com <callcraft456@gmail.com>
2019-06-29 21:18:23 +0300

    Added a constructor to GameInstanceManager

--- src/me/theonlyartz/oitc/managers/GamesManager.java
+++ src/me/theonlyartz/oitc/managers/GamesManager.java
@@ -1,4 +1,17 @@
 package me.theonlyartz.oitc.managers;
 
+import me.theonlyartz.oitc.Main;
+import org.bukkit.Location;
+
+import java.util.ArrayList;
+
 public class GamesManager {
+    public ArrayList<ArrayList<Location>> reservedCords = new ArrayList<ArrayList<Location>>();
+    public ArrayList<ArrayList<Location>> availableCords = new ArrayList<ArrayList<Location>>();
+    public ArrayList<GameInstanceManager> runningGames = new ArrayList<>();
+    private Main plugin;
+
+    public GamesManager(Main plugin) {
+        this.plugin = plugin;
+    }
 }
